548b875fc449fd207c27a28644ce2d9d7316363f
Admin <Admin@Admin-PC>
2020-04-13 17:07:56 +0530

    abc

--- A/src/qsp/P2.java
+++ A/src/qsp/P2.java
@@ -1,10 +1,16 @@
 package qsp;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class P2 {
 
-	public static void main(String[] args) {
-		System.out.println("from p2");
-
+	@Test
+	public static void test(){
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.close();
 	}
 
 }
